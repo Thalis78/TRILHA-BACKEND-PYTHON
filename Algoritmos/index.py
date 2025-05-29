3e18f6c0a44234ec021bdfd9b68ab640426e9ea8
Thalisson <thalissonmoura138@gmail.com>
2025-05-29 12:50:57 -0300

    :sparkles:feat:implementa funções, loops, arrays e tratamento de exceções

--- Algoritmos/index.py
+++ Algoritmos/index.py
@@ -1,3 +1,6 @@
+def media(n1, n2):
+    return (n1 + n2) / 2
+
 def resultadoComIf(mediaFinal):
     if mediaFinal >= 7:
         return "APROVADO"
@@ -22,12 +25,73 @@ def resultadoComSwitch(mediaFinal):
         case _:
             return "REPROVADO"
 
-def media(n1, n2):
-    return (n1 + n2) / 2
+def exemplo_loops():
+    for i in range(5):
+        print(i + 1)
+    
+    print()
+
+    frutas = ['maçã', 'banana', 'laranjas']
+    for fruta in frutas:
+        print(fruta)
+        print(i)
+    
+    print()
+
+    for indice, fruta in enumerate(frutas):
+        print(f"{indice}: {fruta}")
+
+    print()
+
+    pessoa = {'nome': 'João', 'idade': 30}
+    for chave, valor in pessoa.items():
+        print(f"{chave} = {valor}")
+
+    print()
+
+    for i in range(3):
+        for j in range(2):
+            print(f"i={i}, j={j}")
+
+    print()
+
+    nomes = ['Ana', 'Pedro', 'João']
+    idades = [25, 30, 22]
+
+    for nome, idade in zip(nomes, idades):
+        print(f"{nome} tem {idade} anos")
+
+    print()
+
+    contador = 0
+    while contador < 5:
+        print(contador + 1)
+        contador += 1
+    else:
+        print("FIM DO LOOP!")
+
+
+def exemplo_arrays():
+    numeros = [1,2,3,4,5]
+    nomes = ['Ana', 'Pedro', 'João']
+
+    numeros[0] = 20
+    nomes.append("Thalisson")
+    nomes.remove("João")
+
+    print(nomes[2])
+    print(numeros[0])
+
+# EU NÃO PRECISO OBRIGATORIAMENTE ESPECIFICAR O TIPO DE EXCEÇÃO EM PYTHON PORÉM ISSO VAI DIFICULTAR O DEBUG.
+def usandoExcecoes():
+    try:
+        numero = int (input("DIGITE UM  NÚMERO?"))
+        print(numero)
+    except:
+        print("Você não digitou um número")
+    finally:
+        print("Programa finalizado!")
 
 def main():
-    print(resultadoComIf(media(10, 7)))
-    print(resultadoComTernario(media(5, 5)))
-    print(resultadoComSwitch(media(4, 3)))
-
+    exemplo_arrays()
 main()
